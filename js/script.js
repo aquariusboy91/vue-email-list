bcade82989134031e67e94a6a37afe20747ed18e
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2022-01-10 15:22:07 +0100

    add elements in list

--- js/script.js
+++ js/script.js
@@ -1,18 +1,14 @@
 var app = new Vue({
     el: '#app',
     data: {
-     email: '',
+     email: [],
     },
     created(){
-        const self = this;
-        axios
-        .get("https://flynn.boolean.careers/exercises/api/random/mail", {
-            params: {
-            items: 10
-        },
-      })
-        .then(function (response) {
-        self.email = response.data.response
-        });
+        for (i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => {
+              this.email.push({ text : response.data.response});
+            });
+          }
       }
   })
